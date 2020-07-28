4c62f42ea77e397606d5970e0e91f039871948e8
jmarijano <jocina.mar@gmail.com>
2020-07-28 22:15:10 +0200

    Changed all schema classes.

--- api/database/schemas.py
+++ api/database/schemas.py
@@ -10,23 +10,25 @@ def initialize_ma(app):
 
 class RoleSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'created_on', 'updated_on')
+        fields = ('id', 'name', 'created_on', 'updated_on')
 
 
 class CountrySchema(ma.Schema):
     class Meta:
-        fields = ('name', 'created_on', 'updated_on')
+        fields = ('id', 'name', 'created_on', 'updated_on')
 
 
 class CitySchema(ma.Schema):
     class Meta:
-        fields = ('name', 'country_id', 'created_on', 'updated_on')
+        fields = ('id', 'name', 'country_id', 'created_on', 'updated_on')
 
 
 class AddressSchema(ma.Schema):
     class Meta:
-        fields=('street_name', 'city_id', 'created_on', 'updated_on')
+        fields = ('id','street_name', 'city_id', 'created_on', 'updated_on')
+
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields= ('name','surname','email','role_id','created_on', 'updated_on')
+        fields = ('id','name', 'surname', 'email',
+                  'role_id', 'created_on', 'updated_on')
