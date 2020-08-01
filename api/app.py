1f836640a8c8795a87c6498934b04c6e706e9ac9
jmarijano <jocina.mar@gmail.com>
2020-08-01 10:07:39 +0200

    Added sqli datasets and model.

--- api/app.py
+++ api/app.py
@@ -5,6 +5,9 @@ from database.schemas import initialize_ma
 import os
 from resources.routes import initialize_routes
 from setup import kae
+import numpy as np
+import pandas as pd
+from database.model.password_strength_model import func
 
 
 app = Flask(__name__)
@@ -19,5 +22,9 @@ initialize_db(app)
 initialize_ma(app)
 initialize_routes(api)
 
+kae()
+
+func()
 if __name__ == '__main__':
+    data = pd.read_csv("datasets/password_strength.csv",',',error_bad_lines=False)
     app.run(debug=True)
--- api/setup.py
+++ api/setup.py
@@ -1,8 +1,129 @@
-import json
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer
+import glob
+import time
+import pandas as pd
+from nltk import ngrams
+from nltk.tokenize import sent_tokenize
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+import tensorflow as tf
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.text import Tokenizer
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import accuracy_score
+from keras.models import load_model
+import pickle
+
+
+
 
 def kae():
-    with open('MOCK_DATA.json') as json_file:
-        data = json.load(json_file)
-        for p in data['data']:
-            print('Name: '+ p['name'])
-            print('Country_id: '+p['country_id'])
+
+    
+    mymodel = tf.keras.models.load_model('my_model_cnn.h5')
+    myvectorizer = pickle.load(open("vectorizer_cnn", 'rb'))
+
+    input_val="i select this sentence but this should not be injection it is like injection but from my point of view it should not be even if i include union in it like before"
+    input_val=clean_data(input_val)
+    input_val=[input_val]
+
+
+
+    input_val=myvectorizer.transform(input_val).toarray()
+    
+    input_val.shape=(1,64,64,1)
+
+    result=mymodel.predict(input_val)
+    print(result)
+
+def clean_data(input_val):
+    
+    input_val=input_val.replace('\n', '')
+    input_val=input_val.replace('%20', ' ')
+    input_val=input_val.replace('=', ' = ')
+    input_val=input_val.replace('((', ' (( ')
+    input_val=input_val.replace('))', ' )) ')
+    input_val=input_val.replace('(', ' ( ')
+    input_val=input_val.replace(')', ' ) ')
+    input_val=input_val.replace('1 ', 'numeric')
+    input_val=input_val.replace(' 1', 'numeric')
+    input_val=input_val.replace("'1 ", "'numeric ")
+    input_val=input_val.replace(" 1'", " numeric'")
+    input_val=input_val.replace('1,', 'numeric,')
+    input_val=input_val.replace(" 2 ", " numeric ")
+    input_val=input_val.replace(' 3 ', ' numeric ')
+    input_val=input_val.replace(' 3--', ' numeric--')
+    input_val=input_val.replace(" 4 ", ' numeric ')
+    input_val=input_val.replace(" 5 ", ' numeric ')
+    input_val=input_val.replace(' 6 ', ' numeric ')
+    input_val=input_val.replace(" 7 ", ' numeric ')
+    input_val=input_val.replace(" 8 ", ' numeric ')
+    input_val=input_val.replace('1234', ' numeric ')
+    input_val=input_val.replace("22", ' numeric ')
+    input_val=input_val.replace(" 8 ", ' numeric ')
+    input_val=input_val.replace(" 200 ", ' numeric ')
+    input_val=input_val.replace("23 ", ' numeric ')
+    input_val=input_val.replace('"1', '"numeric')
+    input_val=input_val.replace('1"', '"numeric')
+    input_val=input_val.replace("7659", 'numeric')
+    input_val=input_val.replace(" 37 ", ' numeric ')
+    input_val=input_val.replace(" 45 ", ' numeric ')
+
+    return input_val
+
+def confusion_matrix(truth,predicted):
+    
+    
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+    
+    for true,pred in zip(truth,predicted):
+        
+        if true == 1:
+            if pred == true:
+                true_positive += 1
+            elif pred != true:
+                false_negative += 1
+
+        elif true == 0:
+            if pred == true:
+                true_negative += 1
+            elif pred != true:
+                false_positive += 1
+            
+    accuracy=accuracy_function(true_positive, true_negative, false_positive, false_negative)
+    precision=precision_function(true_positive, false_positive)
+    recall=recall_function(true_positive, false_negative)
+    
+    return (accuracy,
+            precision,
+           recall)
+
+def accuracy_function(tp,tn,fp,fn):
+    
+    accuracy = (tp+tn) / (tp+tn+fp+fn)
+    
+    return accuracy
+
+def precision_function(tp,fp):
+    
+    precision = tp / (tp+fp)
+    
+    return precision
+
+def recall_function(tp,fn):
+        
+    recall=tp / (tp+fn)
+    
+    return recall
